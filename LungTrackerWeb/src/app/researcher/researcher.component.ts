61136246d698ffa8ff18c6a13572a9089ae8adf7
roger.homs@upc.edu <roger.homs@upc.edu>
2022-05-08 12:42:22 +0200

    charts

--- LungTrackerWeb/src/app/researcher/researcher.component.ts
+++ LungTrackerWeb/src/app/researcher/researcher.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/UserService';
 import {MatTableModule} from '@angular/material/table'
 import{Person} from '../Person'
-import { ChartModule } from 'angular2-chartjs';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
 
 @Component({
@@ -16,86 +16,100 @@ export class ResearcherComponent implements OnInit {
   constructor(public userService : UserService) { }
   persons :any=[];
   mainResidence :any=[];
+  showGraphs = [false,false,false,false];
   typeLocalitzation = 'pie';
   dataLocalitzation = {
-
-  
-    labels: ["Barcelona", "Vic", "Tarragona", "Girona", "Lleida"],
-  datasets: [
-    {
-      label: "Donde viven los pacientes",
-      fill:'true',
-      backgroundColor: ['#ff0266','#81c784','#29b6f6','#8a3ab9', "#bc2a8d"],
-      data:[10,20,30,40, 15]
-    }
-  ]
-};
-optionsLocalitzation = {
-  legend: {
-    display: true
-},
+    labels: [],
+    datasets: [
+      {
+        fill:'true',
+        backgroundColor: [],
+        data:[]
+      }
+    ]
+  };
+  optionsLocalitzation = {
+    legend: {
+      display: true,
+      position: 'bottom'
+    },
   responsive: true,
   maintainAspectRatio: false,
- 
+  title: {
+    display: true,
+    text: 'Localización de los pacientes',       
+  },
 }
 
-typeCancerType = 'bar';
-dataTypes = {
-  labels: ["Barcelona", "Vic", "Tarragona", "Girona", "Lleida"],
+  typeCancerType = 'bar';
+  dataTypes = {
+ 
+  labels: ["Tipo1", "Tipo2", "Tipo3", "Tipo4", "Tipo5"],
   datasets: [
-    {
-      fill:'true',
-      backgroundColor: ['#ff0266','#81c784','#29b6f6','#8a3ab9', '#bc2a8d'],
-      data:[10,20,30,40,15]
-    }
-  ]
+      {
+  
+        fill:'true',
+        backgroundColor: ['#ff0266','#81c784','#29b6f6','#8a3ab9', '#bc2a8d'],
+        data:[10,20,30,40,15]
+      }
+    ]
   };
   optionsTypes = {
-      legend: {
-        display: true
-    },
       responsive: true,
-      maintainAspectRatio: false,
+      legend: {
+        display: false,
+        position: 'bottom'
+      },
+      title: {
+        display: true,
+        text: 'Tipos de tumor',       
+      },
       scales: {
         xAxes: [
-          { display: true,
-                    gridLines: {
-                        display:false
-                    }
+          { 
+            display: true,
+            gridLines: {
+                display:false
+            }
                 }],
         yAxes: [{
             display: true,
             gridLines: {
                 display:false
-            }   
+            },
+            ticks : {
+              beginAtZero : true
+            }      
         }]
   }
   }
 
-
-
-
   ngOnInit(): void {
 
     this.getTableData();
+    this.getLocalitzationChart();
   }
   getTableData(){
     this.userService.getTableData().subscribe((res)=>{
       this.persons=res;
-      this.getActualResidence(this.persons);
      
     });
   }
-  getActualResidence(persons: Array<Person>) {
-    for(let p in persons){
-      let per:any=p;
-      for(var lP in per.livingplaces){
-        let livingPlace:any=lP;
-        if(livingPlace.isPresent){
-          this.mainResidence.add(livingPlace)
-        }
-      }
-    } 
+
+  getLocalitzationChart(){
+    this.userService.getLocalitzation().subscribe((res: any)=>{
+      this.treatResponseForChart(res);
+
+    })
   }
+  treatResponseForChart(res: any) {
+    for(let i=0; i<res.length; i++){
+      this.dataLocalitzation.labels.push(res[i]["_id"])
+      this.dataLocalitzation.datasets[0].data.push(res[i]["sum"])
+      this.dataLocalitzation.datasets[0].backgroundColor.push("#"+Math.floor(Math.random()*16777215).toString(16))
+    }
+    this.showGraphs[0]=true;
+  }
+
 
 }
