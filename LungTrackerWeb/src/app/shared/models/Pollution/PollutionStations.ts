393896edfb4f3dbe1ca77162b82944132c7c4517
Daniel <danielvequi@gmail.com>
2022-05-17 22:24:25 +0200

    Create new PollutionStations models

---
+++ LungTrackerWeb/src/app/shared/models/Pollution/PollutionStations.ts
@@ -0,0 +1,12 @@
+import {Station} from './Station';
+
+export default class PollutionStations {
+  arsenic: Station;
+  benzopyrene: Station;
+  cadmium: Station;
+  lead: Station;
+  nickel: Station;
+  radon: Station;
+  pm2_5: Station;
+  pm10: Station;
+}
