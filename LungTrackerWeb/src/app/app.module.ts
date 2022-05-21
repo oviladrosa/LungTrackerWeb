a2539da259dbe52369a440fb81882920482cf14d
Daniel <danielvequi@gmail.com>
2022-05-21 16:48:58 +0200

    Add new Maps view to the project modules

--- LungTrackerWeb/src/app/app.module.ts
+++ LungTrackerWeb/src/app/app.module.ts
@@ -38,6 +38,11 @@ import { PatientContaminantsStationsComponent } from './patient-contaminants-sta
 import {MatExpansionModule} from '@angular/material/expansion';
 import { NgxPaginationModule } from 'ngx-pagination';
 import {PatientsContaminantsStationsService} from './patient-contaminants-stations/patients-contaminants-stations.service';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletMapComponent } from './researcher/researcher-map/leaflet-map/leaflet-map.component';
+import { MarkerService } from './researcher/researcher-map/leaflet-map/marker.service';
+import { PopupService } from './researcher/researcher-map/leaflet-map/popup.service';
+import { ResearcherMapComponent } from './researcher/researcher-map/researcher-map.component';
 
 
 
@@ -51,7 +56,9 @@ import {PatientsContaminantsStationsService} from './patient-contaminants-statio
     LoginComponent,
     ResearcherComponent,
     MapsComponent,
-    PatientContaminantsStationsComponent
+    PatientContaminantsStationsComponent,
+    LeafletMapComponent,
+    ResearcherMapComponent
   ],
   imports: [
     BrowserModule,
@@ -80,12 +87,15 @@ import {PatientsContaminantsStationsService} from './patient-contaminants-statio
     MatSnackBarModule,
     ChartModule,
     MatExpansionModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    LeafletModule
   ],
   providers: [
     LungFormQuestionsService,
     HomeComponentService,
-    CookieService
+    CookieService,
+    MarkerService,
+    PopupService
   ],
   bootstrap: [AppComponent]
 })
