2686dc5422cb2835ee9c10f8a9ebaa50e2aac365
Daniel <danielvequi@gmail.com>
2022-05-21 16:50:13 +0200

    Create /researchers/maps view with Leaflet component

---
+++ LungTrackerWeb/src/app/researcher/researcher-map/researcher-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-researcher-map',
+  templateUrl: './researcher-map.component.html',
+  styleUrls: ['./researcher-map.component.css']
+})
+export class ResearcherMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
